c847a36604bc3ce198f16704f25fb8c38ef7b29a
aniketsachan738044-a11y <aniketsachan738044@gmail.com>
2026-07-24 06:38:06 +0530

    Time: 134 ms (97.75%), Space: 35.7 MB (76.97%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    int uniqueXorTriplets(std::vector<int>& nums) {
-        std::unordered_set<int> unique_pairs;
-        std::unordered_set<int> unique_triplets;
-        
-        // 1. Precompute and store all unique XOR values of pairs (i, j) where i <= j
-        for (size_t i = 0; i < nums.size(); ++i) {
-            for (size_t j = i; j < nums.size(); ++j) {
-                unique_pairs.insert(nums[i] ^ nums[j]);
-            }
+    int uniqueXorTriplets(vector<int>& nums) {
+        static bool present[1501];
+        memset(present, 0, sizeof(present));
+        for (int x : nums) present[x] = true;
+
+        int vals[1501], vn = 0;
+        for (int v = 1; v <= 1500; v++)
+            if (present[v]) vals[vn++] = v;
+
+        static bool pairXor[2048];
+        memset(pairXor, 0, sizeof(pairXor));
+        for (int i = 0; i < vn; i++)
+            for (int j = 0; j < vn; j++)
+                pairXor[vals[i] ^ vals[j]] = true;
+
+        static bool tripleXor[2048];
+        memset(tripleXor, 0, sizeof(tripleXor));
+        for (int v = 0; v < 2048; v++) {
+            if (!pairXor[v]) continue;
+            for (int i = 0; i < vn; i++)
+                tripleXor[v ^ vals[i]] = true;
         }
-        
-        // 2. Compute combinations of (pair_xor) ^ nums[k]
-        for (int p_xor : unique_pairs) {
-            for (int val : nums) {
-                unique_triplets.insert(p_xor ^ val);
-            }
-        }
-        
-        return unique_triplets.size();
+
+        int count = 0;
+        for (int v = 0; v < 2048; v++) count += tripleXor[v];
+        return count;
     }
 };
