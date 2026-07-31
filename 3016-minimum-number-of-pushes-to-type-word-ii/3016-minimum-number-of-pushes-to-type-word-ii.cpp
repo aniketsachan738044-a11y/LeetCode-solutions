1a58846a6bfd329bd028597441388c0848cae5dc
aniketsachan738044-a11y <aniketsachan738044@gmail.com>
2026-07-31 08:37:47 +0530

    Time: 7 ms (69.76%), Space: 28.5 MB (25.39%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> freq(26, 0);
+        for (char c : word) freq[c - 'a']++;
+        
+        sort(freq.rbegin(), freq.rend());
+        
+        int total = 0;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0) break;
+            int presses = i / 8 + 1;
+            total += presses * freq[i];
+        }
+        return total;
+    }
+};
