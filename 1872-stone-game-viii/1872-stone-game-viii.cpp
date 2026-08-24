add9fabf8d0450ad8ab1f772df83ef165f6224c8
aniketsachan738044-a11y <aniketsachan738044@gmail.com>
2026-08-24 16:24:34 +0530

    Time: 99 ms (80.97%), Space: 87 MB (99.12%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+        for (int i = 1; i < n; i++) stones[i] += stones[i - 1]; // prefix sums in place
+
+        int dp = stones[n - 1];
+        for (int i = n - 2; i >= 1; i--) {
+            dp = max(dp, stones[i] - dp);
+        }
+        return dp;
+    }
+};
